78b81141f3e8400cbfd2d82b2e54939816c0b2ff
And42 <andrewskobcov@yandex.ru>
2019-02-07 23:49:35 +0300

    Refactoring

--- AndroidHelperTests/MainSmaliTests.cs
+++ AndroidHelperTests/MainSmaliTests.cs
@@ -7,19 +7,19 @@ namespace AndroidHelperTests
     public class MainSmaliTests
     {
         [Fact]
-        public void MainSmaliFirst()
-        {
-            MainSmali cls = new MainSmali(Path.Combine(Paths.AdditFilesFolder, "col.smali"), "onCreate");
-
-            Assert.Equal(65642, cls.Index); // "onCreate  index"
-        }
-
-        [Fact]
         public void OnCreateInsideMethod()
         {
             var mainSmali = new MainSmali(Path.Combine(Paths.AdditFilesFolder, "1.smali"), "onCreate");
 
             Assert.Equal(2161, mainSmali.Index);
+        }
+
+        [Fact]
+        public void MainSmaliFirst()
+        {
+            var mainSmali = new MainSmali(Path.Combine(Paths.AdditFilesFolder, "2.smali"), "onCreate");
+
+            Assert.Equal(65642, mainSmali.Index);
         }
     }
 }
